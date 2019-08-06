25a912e04486553a080b441333ecd639e8a5f648
CorentinDeblock <corentindeblock@gmail.com>
2019-08-06 22:10:00 +0200

    Updates

---
+++ precache-manifest.cfd3026839c325dc1603d494299e4e4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be54541a74129021de9d6f3bbd10e51",
+    "url": "/establishement/index.html"
+  },
+  {
+    "revision": "c364cb03062208d78ed1",
+    "url": "/establishement/static/css/main.1d9afa19.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/establishement/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "c364cb03062208d78ed1",
+    "url": "/establishement/static/js/main.4b352f5e.chunk.js"
+  },
+  {
+    "revision": "bf2e0dd51314068f62e1",
+    "url": "/establishement/static/js/runtime~main.05aa7e6a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/establishement/static/media/logo.ee7cd8ed.svg"
+  }
+]);
